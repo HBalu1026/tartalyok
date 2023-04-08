9cd7e1374e5e14ba2fc37e85d1254b15b37314e3
HBalu1026 <71315012+HBalu1026@users.noreply.github.com>
2023-04-08 18:25:49 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tartaly nagytartaly, literes;
+            
+            Console.WriteLine(nagytartaly.Info());
+            nagytartaly.Tolt(12500);
+            nagytartaly.Tolt(5680);
+            Console.WriteLine(nagytartaly.Info());
+            Console.WriteLine();
+            Console.WriteLine(literes.Info());
+            literes.DuplazMeretet();
+            literes.Tolt(0.56);
+            Console.WriteLine(literes.Info());
+            literes.TeljesLeengedes();
+            literes.Tolt(1.6);
+            Console.WriteLine(literes.Info());
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Tartaly.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Tartaly
+    {
+        string nev;
+        int a;
+        int b;
+        int c;
+        double aktLiter;
+
+        public Tartaly(string nev, int a, int b, int c)
+        {
+            this.nev = nev;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.aktLiter = 0;
+        }
+        public Tartaly(String nev)
+        {
+            this.nev = nev;
+            this.a = 10;
+            this.b = 10;
+            this.c = 10;
+            this.aktLiter = 0;
+        }
+        public double Terfogat
+        {
+            get { return this.a * this.b * this.c; }
+        }
+        public void DuplazMeretet()
+        {
+            this.a *= 2;
+        }
+        public void TeljesLeengedes()
+        {
+            this.aktLiter = 0;
+        }
+        public double Toltottseg
+        {
+            get { return this.aktLiter / (Terfogat / 100); }
+        }
+        public void Tolt(double hozzaad)
+        {
+            if (this.aktLiter +hozzaad > Terfogat)
+            {
+                Console.WriteLine("Túlcsordulás! Ennyit nem lehet hozzáadni!");
+            }
+            else
+            {
+                this.aktLiter += hozzaad;
+            }
+            /*
+            try
+            {
+                if ( this.aktLiter + hozzaad <= Terfogat)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Túlcsordulás! Ennyit nem lehet hozzáadni!");
+            }
+            */
+        }
+        public string Info()
+        {
+            return String.Format("{0}: {1} cm3 = ({6} liter), töltöttsege:{2}%, ({7}" +
+            "liter), méretei:{3}x{4}x{5} cm", this.nev, this.Terfogat,
+            this.Toltottseg * 100, this.a, this.b, this.c, this.Terfogat / 1000, this.aktLiter);
+        }
+    }
+}
